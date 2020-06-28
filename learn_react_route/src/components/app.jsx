ae8dd78b0b2f290521a2939febaf664fcca19dac
cqj_9412 <978390012@qq.com>
2020-06-28 09:56:34 +0800

    学习路由

---
+++ learn_react_route/src/components/app.jsx
@@ -0,0 +1,36 @@
+import React from 'react'
+import {NavLink, Switch, Route, Redirect} from 'react-router-dom'
+import About from '../views/about'
+import Home from '../views/home'
+export default class App extends React.Component{
+  render() {
+    return (
+      <div style={{padding: '50px'}}>
+        <div className="row">
+          <div className="col-xs-offset-2 col-xs-8">
+            <div className="page-header"><h2>React Router Demo</h2></div>
+          </div>
+        </div>
+        <div className="row">
+          <div className="col-xs-2 col-xs-offset-2">
+            <div className="list-group">
+              <NavLink className="list-group-itme" to='/about'>About</NavLink>
+              <NavLink className="list-group-itme" to='/home'>home</NavLink>
+            </div>
+          </div>
+          <div className="col-xs-6">
+            <div className="panel">
+              <div className="panel-body">
+                <Switch>
+                  <Route path="/about" component={About} />
+                  <Route path="/home" component={Home} />
+                  <Redirect to='/about'/> 
+                </Switch>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    )
+  }
+}
--- learn_react_route/src/index.js
+++ learn_react_route/src/index.js
@@ -1,13 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import App from './components/app';
 import * as serviceWorker from './serviceWorker';
+import {BrowserRouter} from 'react-router-dom'
+import 'bootstrap/dist/css/bootstrap.min.css'
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  (
+    <BrowserRouter>
+      <React.StrictMode>
+        <App />
+      </React.StrictMode>
+    </BrowserRouter>
+  ),
   document.getElementById('root')
 );
 
